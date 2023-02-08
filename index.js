209aa997774772e570c276563af8ec42e12b41cc
Osei Rick <rickoseiampah@gmail.com>
2023-02-08 14:58:48 +0300

    extra security packages added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 require("express-async-errors");
 
 // extra security package
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const connect = require("./db/connect");
 const auth = require("./middlewares/auth");
@@ -15,13 +19,25 @@ const jobsRouter = require("./routes/jobs_routes");
 const authRouter = require("./routes/auth_routes");
 
 // middleware
-server.use(express.json());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
+server.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+server.get("/", (req, res) => {
+  res.send("Jobs API");
+});
 // routes
 server.use("/api/v1/auth/", authRouter);
 server.use("/api/v1/jobs/", auth, jobsRouter);
-
-// middlewares
 
 // error handler middleware
 server.use(notFound);
